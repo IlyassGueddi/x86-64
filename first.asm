fc74da3da1541576f66dc4d6404db807c8698754
Mial <gueddiilyass99@gmail.com>
2026-02-13 10:29:45 +0100

    asm

--- first.asm
+++ first.asm
@@ -1,1 +1,12 @@
-mov 
+section .text
+global _start
+
+_start:
+    mov rax, 5
+    mov rbx, 3
+    add rax, rbx
+
+    mov rax, 60        ; syscall: exit
+    mov rdi, 0         ; status = 0
+    syscall
+
